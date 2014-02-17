ee83387637928844c699ea6019e411df1673d38a
dshi7 <shidaohang1990@gmail.com>
2014-02-17 01:05:40 -0600

    basic pipe finished

--- p2/single.c
+++ p2/single.c
@@ -19,7 +19,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void  executeAtomicCommand(pid_t* child_pid, int read_filedes, int write_filedes, char* cmd_line) {
+void  executeAtomicCommand(pid_t* child_pid, int read_filedes, int write_filedes, char* cmd_line)  {
+  //void  executeAtomicCommand(pid_t* child_pid, int* read_filedes, int* write_filedes, char* cmd_line, int prev_pipe_exist, int* prev_pipe_wr, int next_pipe_exist, int* next_pipe_rd ) {
 
   char* sgl_cmd_argv[512];
   /* must be a non-built-in command */
@@ -29,22 +30,13 @@ void  executeAtomicCommand(pid_t* child_pid, int read_filedes, int write_filedes
 
   *child_pid = fork();
 
-  if ( *child_pid==-1 ) {
-
-    /* fork error */
-    perror("Cannot create a child process\n");
-    exit(EXIT_FAILURE);
-
-  }
-  else if ( *child_pid==0 ) {
+  if(*child_pid==0) {
 
     /* in the child process */
 
-    if(read_filedes!=-1)
-      dup2(read_filedes, STDIN_FILENO);
+    dup2(read_filedes, STDIN_FILENO);
 
-    if(write_filedes!=-1)
-      dup2(write_filedes, STDOUT_FILENO);
+    dup2(write_filedes, STDOUT_FILENO);
 
 
     /* if false
@@ -54,6 +46,13 @@ void  executeAtomicCommand(pid_t* child_pid, int read_filedes, int write_filedes
 
     /* kill the child process */
     _exit(EXIT_SUCCESS);
+  }
+  else if(*child_pid==-1) {
+
+    /* fork error */
+    perror("Cannot create a child process\n");
+    _exit(EXIT_FAILURE);
+
   }
 }
 
@@ -67,10 +66,11 @@ void  executeSingleCommand(pid_t* child_pid, char* cmd_line) {
   char  output_file[512];
   char* file_redir_argv[512];
   char* pipe_argv[512];
-  //  char* sgl_cmd_argv[512];
   //  char  cwd[512];
-  int   redir_filedes = -1;
-
+  int   redir_fd = -1;
+  /* used for pipe */
+  int   pipe_num;
+  int   pipe_fd[512*2];        
 
   /* parse the file redirection */
   parseFileDirection(cmd_line, file_redir_argv);
@@ -86,70 +86,72 @@ void  executeSingleCommand(pid_t* child_pid, char* cmd_line) {
     strcpy(output_file, file_redir_argv[1]);
     char  *token = strtok(output_file, " \t");
     strcpy(output_file, token);
-    redir_filedes = open(output_file, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+    redir_fd = open(output_file, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
   }
 
 
 
   if(strchr(cmd_line, '|')) {
 
-    /* parse the pipe */
-    parsePipe(cmd_line, pipe_argv);
-
     int j=0;
-    while(pipe_argv[j])
-      printf("%s\n", pipe_argv[j++]);
+    /* parse the pipe and the number of pipes are returned */
+    pipe_num = parsePipe(cmd_line, pipe_argv);
 
-    exit(EXIT_SUCCESS);
+    /* the number of atomic commands connected with pipes = pipe_num+1
+     *  denoted by pipe_argv[0] , ... , pipe_argv[pipe_num] */
+
+
+    /* create (pipe_num+1) different pipes */
+    for(j=0; j<pipe_num; j++) 
+      if (pipe(pipe_fd+(j<<1))==-1) 
+        exit(EXIT_FAILURE);
+
+    /* execute all the atomic commands */
+    for(j=0; j<=pipe_num; j++) {
+
+      if( fork()==0 ) {
+
+        char* sgl_cmd_argv[512];
+        /* control the previous pipe */
+        if(j!=0) {
+          close(pipe_fd[2*j-1]);
+          dup2(pipe_fd[2*j-2], STDIN_FILENO);
+          close(pipe_fd[2*j-2]);
+        }
+
+        /* control the next pipe */
+        if(j!=pipe_num) {
+          close(pipe_fd[2*j+0]);
+          dup2(pipe_fd[2*j+1], STDOUT_FILENO);
+          close(pipe_fd[2*j+1]);
+        }
+
+        /* parse the single command */
+        parseSingleCommand(pipe_argv[j], sgl_cmd_argv);
+
+        /* execute the command */
+        execvp(sgl_cmd_argv[0], sgl_cmd_argv);
+
+        _exit(EXIT_FAILURE);
+
+      }
+    }
+
+    for(j=0; j<pipe_num; j++) {
+      close(pipe_fd[2*j+0]);
+      close(pipe_fd[2*j+1]);
+    }
+
+    for(j=0; j<=pipe_num; j++)
+      wait(NULL);
+
 
   }
   else {
 
-    executeAtomicCommand(child_pid, STDIN_FILENO, redir_filedes==-1 ? STDOUT_FILENO : redir_filedes, file_redir_argv[0]);
+    executeAtomicCommand(child_pid, STDIN_FILENO, redir_fd==-1 ? STDOUT_FILENO : redir_fd, file_redir_argv[0]);
 
   }
-  //    else if ( *child_pid==0 ) {
-  //
-  //      /* in the child process */
-  //
-  //      /* duplicate file descriptor if file redirection is required */
-  //      if (file_redir_argv[1]) {
-  //        file_descriptor = open(output_file, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-  //      }
-  //
-  //      if(file_descriptor!=-1)
-  //        dup2(file_descriptor, STDOUT_FILENO);
-  //
-  //      /* check if the command is built-in */
-  //      if(isBuiltInCommand(sgl_cmd_argv)) {
-  //
-  //        /* if true
-  //         *  then execute the built-in command */
-  //        if(!strcmp(sgl_cmd_argv[0],"pwd")) {
-  //          if(getcwd(cwd,sizeof(cwd))) 
-  //            fprintf(stdout, "%s\n", cwd);
-  //          else
-  //            printErrorMsg();
-  //        }
-  //        else if(!strcmp(sgl_cmd_argv[0],"cd")) {
-  //
-  //        }
-  //      }
-  //      else {
-  //
-  //        /* if false
-  //         *  execute the command line by calling execvp */
-  //        execvp(sgl_cmd_argv[0], sgl_cmd_argv);
-  //
-  //      }
-  //
-  //      if(file_descriptor!=-1)
-  //        close(file_descriptor);
-  //
-  //
-  //      /* kill the child process */
-  //      _exit(EXIT_SUCCESS);
-  //    }
 }
 
 
--- p2/single.h
+++ p2/single.h
@@ -3,10 +3,14 @@
 
 #include<unistd.h>
 
+#define PIPE_RD_END 0
+#define PIPE_WR_END 1
+
 /* execute an atomic command in shell
  *  child process id, read/write file descriptors are also specified in this function
  *  */ 
 void  executeAtomicCommand(pid_t* child_pid, int read_filedes, int write_filedes, char* cmd_line);
+//void  executeAtomicCommand(pid_t* child_pid, int* read_filedes, int* write_filedes, char* cmd_line, int prev_pipe_exist, int* prev_pipe_wr, int next_pipe_exist, int* next_pipe_rd ); 
 
 
 /* execute a single command */ 
